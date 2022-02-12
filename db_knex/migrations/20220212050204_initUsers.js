a93fbeb0332b4bbbbb3b4fba2fbb2139e69c29ea
Noah Vandenberg <noah@noahvandenberg.com>
2022-02-12 10:10:54 -0800

    fakeUser object now has contextual emails

--- db_knex/migrations/20220212050204_initUsers.js
+++ db_knex/migrations/20220212050204_initUsers.js
@@ -10,7 +10,6 @@ exports.up = function(knex) {
       .primary();
     table
       .string("email")
-      .unique()
       .notNullable();
       table.string('first_name').notNullable();
       table.string('last_name').notNullable();
--- db_knex/seeds/addUsers.js
+++ db_knex/seeds/addUsers.js
@@ -4,16 +4,22 @@
  */
 
  const { faker } = require('@faker-js/faker');
+ faker.setLocale('nl');
 
-const createFakeUser = () => ({
-    email: faker.internet.email(),
-    first_name: faker.name.firstName(),
-    last_name: faker.name.lastName()
-});
+const createFakeUser = () => {
+  const last_name = faker.name.lastName();
+  const first_name = faker.name.firstName();
+  const email = faker.internet.email(first_name,last_name)
+  return {
+    email: email,
+    first_name: first_name,
+    last_name: last_name
+  };
+}
 
 exports.seed = async function(knex) {
   const fakeUsers = [];
-  const desiredFakeUsers = 50;
+  const desiredFakeUsers = 10;
   for (let i = 0; i < desiredFakeUsers; i++) {
     fakeUsers.push(createFakeUser());
   }
