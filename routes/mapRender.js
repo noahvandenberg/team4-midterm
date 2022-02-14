0cd6b8f7699a315d67d746fff9370ce1842f9757
Thomas Ngo <thomasngo818@gmail.com>
2022-02-14 15:50:41 +0000

    Fix error in route handler path so it correctly listens at /

--- routes/mapRender.js
+++ routes/mapRender.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = () => {
-  router.get("/map", (req, res) => {
+
+  router.get("/", (req, res) => {
     console.log("In the user maps route.");
     res.render('map')
   });
+
   return router;
 };
