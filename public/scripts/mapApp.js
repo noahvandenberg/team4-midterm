375eed0d67fec3720bde77504cd149a9eacbd1b8
Thomas Ngo <thomasngo818@gmail.com>
2022-02-14 16:18:24 +0000

    Add map page scripting file with map render function

---
+++ public/scripts/mapApp.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  const renderMap = function() {
+    // Sets Map and it's initial location
+    var map = L.map('map').setView([49.13068254312452, -123.14892282607158], 13);
+
+    // Grabs titles (background images) for mapbox
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1Ijoibm9haHZhbmRlbmJlcmciLCJhIjoiY2t6ajRtazg2MGs5bjJwbnlveTFoN2cwaSJ9.fzC2TyqKFe8sidsYJlVzdQ'
+    }).addTo(map);
+  };
+
+  renderMap();
+});
