a8c1d0a6a7c2dceaaa15d70c37b9e642d2608720
Darcy <darcy.lauder@shaw.ca>
2022-02-16 11:21:16 -0700

    add map to navigation page

--- public/scripts/navigation.js
+++ public/scripts/navigation.js
@@ -4,13 +4,24 @@ $(() => {
     if ($('#mySidenav').width() === 10) {
       console.log(document.cookie);
       $.get('/maps/8', (maps, status) => console.log(status, maps));
-      $('#mySidenav').width('300px');
-    } else $('#mySidenav').width('10px');
+      $('#mySidenav').width('200px');
+      $('#openEditPane').toggle();
+    } else {
+      $('#mySidenav').width('10px');
+      $('#openEditPane').toggle();
+    }
+  });
+
+  $('#openEditPane').click(() => {
+    if ($('#myEditnav').width() === 0) {
+      $('#myEditnav').width('200px');
+    } else $('#myEditnav').width('0px');
   });
 
   $('#myEditnav').click(() => {
     if ($('#myEditnav').width() === 0) {
-      $('#myEditnav').width('300px');
-    } else $('#myEditnav').width('10px');
+      $('#myEditnav').width('200px');
+    } else $('#myEditnav').width('0px');
   });
+
 });
