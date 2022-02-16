d7420720562e93f19b206c3c94f69ec8b97b52a9
Darcy <darcy.lauder@shaw.ca>
2022-02-15 20:58:03 -0700

    sidenav.js

--- public/scripts/sidenav.js
+++ public/scripts/sidenav.js
@@ -1,9 +1,13 @@
+const { findUserMaps } = require('../../db/map-queries');
+
 $(() => {
 
   $('#mySidenav').click(() => {
     console.log($('#mySidenav').width());
     if ($('#mySidenav').width() === 10) {
       $('#mySidenav').width('300px');
+      let maps = findUserMaps();
+      console.log(maps);
     } else $('#mySidenav').width('10px');
   });
 });
