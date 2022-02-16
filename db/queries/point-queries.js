ec22a8b05089c208d82d3043d1b7f371130e2522
Noah V <noah@noahvandenberg.com>
2022-02-15 16:55:10 -0800

    refactor to async/await

--- db/queries/point-queries.js
+++ db/queries/point-queries.js
@@ -1,55 +1,52 @@
 const db = require('../db');
 
-
-const allPoints = function() {
+// Gets all points in DB
+const allPoints = async () => {
   const strQuery = `SELECT * FROM points;`;
-  return db
-    .query(strQuery)
-    .then(result => result.rows)
-    .catch(err => console.log('Error:', err.message));
+  const dbResponse = await db.query(strQuery)
+  return dbResponse.rows
 };
 exports.allPoints = allPoints;
 
 
-const mapPointsByUser = function(userId) {
+// Gets all points for a user
+const mapPointsByUser = async (userId) => {
   const values = [userId];
   const strQuery = `
     SELECT * FROM points
     WHERE creator_id = $1;`;
-  return db
-    .query(strQuery, values)
-    .then(result => result.rows)
-    .catch(err => console.log('Error:', err.message));
+  const dbResponse = await db.query(strQuery,values)
+  return dbResponse.rows
 };
 exports.mapPointsByUser = mapPointsByUser;
 
 
-const mapPoints = function(mapId) {
+// Gets all points for a map
+const mapPoints = async (mapId) => {
   const values = [mapId];
   const strQuery = `
     SELECT * FROM points
     JOIN maps_points ON point_id = points.id
     JOIN maps ON map_id = maps.id
     WHERE map_id = $1;`;
-  return db
-    .query(strQuery, values)
-    .then(result => result.rows)
-    .catch(err => console.log('Error:', err.message));
+  const dbResponse = await db.query(strQuery,values)
+  return dbResponse.rows
 };
 exports.mapPoints = mapPoints;
 
 
-const findPoint = function(pointId) {
+//
+const findPoint = async (pointId) => {
   const values = [pointId];
   const strQuery = `SELECT * FROM points WHERE id = $1;`;
-  return db
-    .query(strQuery, values)
-    .then(result => result.rows[0])
-    .catch(err => console.log('Error:', err.message));
+  const dbResponse = await db.query(strQuery,values)
+  return dbResponse.rows
 };
 exports.findPoint = findPoint;
 
-const editPoint = function(user, body) {
+
+//
+const editPoint = async (user, body) => {
   const values = Object.values(body);
   let strQuery = 'UPDATE points SET ';
   switch (values.shift()) {
@@ -66,37 +63,32 @@ const editPoint = function(user, body) {
   strQuery += `$1
     WHERE id = $2
     RETURNING *;`;
-  return db
-    .query(strQuery, values)
-    .then(result => result.rows[0])
-    .catch(err => console.log('Error:', err.message));
+  const dbResponse = await db.query(strQuery,values)
+  return dbResponse.rows
 };
 exports.editPoint = editPoint;
 
-
-const newPoint = function(point) {
+// Creates a new map point
+const newPoint = async (point) => {
   const values = [point.creator_id, point.title, point.description, point.image_url, point.latitude, point.longitude];
   const strQuery = `
     INSERT INTO points (creator_id, time_created, title, description, image_url, latitude, longitude)
     VALUES ($1, Date.now(), $2, $3, $4, $5, $6)
     RETURNING *;`;
-  return db
-    .query(strQuery, values)
-    .then(result => result.rows[0])
-    .catch(err => console.log('Error:', err.message));
+  const dbResponse = await db.query(strQuery,values)
+  return dbResponse.rows[0]
 };
 exports.newPoint = newPoint;
 
 
-const deletePoint = function(point) {
+// Creates a new map point
+const deletePoint = async (point) => {
   const values = [point];
   const strQuery = `
       DELETE FROM points
       WHERE id = $1
       RETURNING *;`;
-  return db
-    .query(strQuery, values)
-    .then(result => result.rows[0])
-    .catch(err => console.log('Error:', err.message));
+    const dbResponse = await db.query(strQuery,values)
+    return dbResponse.rows[0]
 };
 exports.deletePoint = deletePoint;
