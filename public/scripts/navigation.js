cee3e98107e24e4524405a1ae087bb849a61a5d6
Darcy <darcy.lauder@shaw.ca>
2022-02-16 20:11:25 -0700

    fix points

--- public/scripts/navigation.js
+++ public/scripts/navigation.js
@@ -3,11 +3,16 @@ $(() => {
   $.get('/maps/8', (maps) => {
     let mapTitles = '';
     for (const map of maps) {
-      mapTitles += '<div class="mapName">' + map.title + '</div>';
+      mapTitles += '<div class="mapName" id="#' + map.id + '">' + map.title + '</div>';
     }
     $('.mapDisplay').html(mapTitles);
     renderMap(maps[0].id);
   });
+
+  $(document).on('click', '.mapName', (function() {
+    console.log($(this).attr('id'));
+    renderMap($(this).attr('id'));
+  }));
 
   $('#openEditPane').click(() => {
     if ($('#myEditnav').width() === 0) {
