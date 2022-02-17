c9bd3f064f8a5c27b29827c80a0af5be109e9dc0
Thomas Ngo <thomasngo818@gmail.com>
2022-02-17 00:33:53 +0000

    Modify renderMap to take a mapId parameter and query the database for that map 's points

--- public/scripts/mapApp.js
+++ public/scripts/mapApp.js
@@ -9,41 +9,66 @@ $(document).ready(function() {
 // Will take an array parameter with coordinates to set initial view
 //    first element will be latitude and second will be longitude
 const renderMap = function(mapId) {
-  // Sets Map and it's initial location
-  const map = L.map('map').setView([49.13068254312452, -123.14892282607158], 13);
+  // hardcoded map for now
+  mapId = 7;
 
-  // Grabs titles (background images) for mapbox
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoibm9haHZhbmRlbmJlcmciLCJhIjoiY2t6ajRtazg2MGs5bjJwbnlveTFoN2cwaSJ9.fzC2TyqKFe8sidsYJlVzdQ'
-  }).addTo(map);
+  return $.ajax({
+    method: "GET",
+    url: `/points/${mapId}`
+  }).then((data) => {
+    // Sets Map and it's initial location
+    const initialLat = data.points[0].latitude;
+    const initialLng = data.points[0].longitude;
+    const map = L.map('map').setView([initialLat, initialLng], 13);
 
-  // diable map zoom on double click
-  map.doubleClickZoom.disable();
+    // create layer group to hold markers
+    const markers = L.layerGroup().addTo(map);
 
-  // create layer group to hold markers
-  const markers = L.layerGroup().addTo(map);
+    // go through points and add them to the map
+    data.points.forEach((point) => {
+      const lat = point.latitude;
+      const lng = point.longitude;
+      const latlng = { lat, lng };
+      const marker = L.marker(latlng).addTo(markers);
+    });
 
-  // handler to add marker to map as well as corresponding point to sidebar list
-  map.on("dblclick", function(pointer) {
+    // Grabs titles (background images) for mapbox
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1Ijoibm9haHZhbmRlbmJlcmciLCJhIjoiY2t6ajRtazg2MGs5bjJwbnlveTFoN2cwaSJ9.fzC2TyqKFe8sidsYJlVzdQ'
+    }).addTo(map);
 
-    console.log(pointer);
+    // diable map zoom on double click
+    map.doubleClickZoom.disable();
 
-    // Add point to map
-    const marker = L.marker(pointer.latlng).addTo(markers);
-    console.log(marker);
-    // database query to create new point goes here
 
-    // Add corresponding point element to sidebar list
-    const $newPoint = $('<input>').attr('type', 'button');
-    $newPoint.attr('name', Math.floor(Math.random() * 1000));
-    $newPoint.val("a new point");
-    $('.points-list').append($newPoint);
-  });
+    // handler to add marker to map as well as corresponding point to sidebar list
+    map.on("dblclick", function(pointer) {
+
+      console.log(pointer);
+
+      // Add point to map
+      const marker = L.marker(pointer.latlng).addTo(markers);
+      console.log(marker);
+
+
+      // // database query to create new point goes here
+
+      // // Add corresponding point element to sidebar list
+      // const $newPoint = $('<input>').attr('type', 'button');
+      // $newPoint.attr('name', Math.floor(Math.random() * 1000));
+      // $newPoint.val("a new point");
+      // $('.points-list').append($newPoint);
+    });
+
+  })
+
+
+
 };
 
 
