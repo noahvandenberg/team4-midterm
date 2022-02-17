022395c8fc07d25973692551b01e231e1305eb9b
Thomas Ngo <thomasngo818@gmail.com>
2022-02-17 19:14:43 +0000

    Update addPointToEditForm helper function to populate all inputs in the form

--- public/scripts/mapApp.js
+++ public/scripts/mapApp.js
@@ -14,10 +14,8 @@ const renderMap = function(mapId) {
     method: "GET",
     url: `/points/${mapId}`
   }).then((data) => {
-    // Sets Map and it's initial location
-    const initialLat = data.points[0].latitude;
-    const initialLng = data.points[0].longitude;
-    const map = L.map('map')//.setView([initialLat, initialLng], 11);
+    // Create map
+    const map = L.map('map');
 
     // create layer group to hold markers
     const markers = L.featureGroup().addTo(map);
@@ -33,7 +31,6 @@ const renderMap = function(mapId) {
       addPointToSidebar(point);
     });
 
-    // set initial view of the map to the points populated
     map.fitBounds(markers.getBounds());
 
     // Grabs titles (background images) for mapbox
@@ -46,7 +43,6 @@ const renderMap = function(mapId) {
       accessToken: 'pk.eyJ1Ijoibm9haHZhbmRlbmJlcmciLCJhIjoiY2t6ajRtazg2MGs5bjJwbnlveTFoN2cwaSJ9.fzC2TyqKFe8sidsYJlVzdQ'
     }).addTo(map);
 
-    // diable map zoom on double click
     map.doubleClickZoom.disable();
 
 
@@ -93,10 +89,15 @@ const addPointToSidebar = function(point) {
 // helper function to populate values in edit sidebar form
 const addPointToEditForm = function(point) {
   const $editForm = $('#myEditnav');
+  $editForm.find('input[name="id"]').val(point.id);
   $editForm.find('input[name="creator_id"]').val(point.creator_id);
   $editForm.find('input[name="map_id"]').val(point.map_id);
   $editForm.find('input[name="latitude"]').val(point.latitude);
   $editForm.find('input[name="longitude"]').val(point.longitude);
+  $editForm.find('input[name="title"]').val(point.title);
+  $editForm.find('input[name="description"]').val(point.description);
+  $editForm.find('input[name="image_url"]').val(point.image_url);
+
 };
 
 // helper function to build marker
@@ -123,5 +124,3 @@ const buildPopup = function(point) {
   `
   return popupString;
 }
-
-// helper function to find the middle of the max and min lat and lng of all map points
