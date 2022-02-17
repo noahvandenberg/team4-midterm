806d46981d8761a5eb849731e33927b69b1a9efe
Noah V <noah@noahvandenberg.com>
2022-02-17 12:59:00 -0800

    add helper function to check if a user is logged in

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,6 +1,6 @@
-const { allMaps, findMapsByUser, findMapById, updateMapTitle, updateMapDescription, createMap, deleteMap } = require('../db/queries/map-queries')
+const { allMaps, findMapsByUser, findMapById, updateMapTitle, updateMapDescription, updateMapImageUrl, createMap, deleteMap } = require('../db/queries/map-queries')
 const { allPoints, findPointsByUser, findPointById, updatePointTitle, updatePointDescription, updatePointImageURL, updatePointLocation, createPoint, deletePoint} = require('../db/queries/point-queries')
-const { allUsers, findUserById, updateUserEmail, updateUserFirstName, updateUserLastName, updateUserPassword, createUser, deleteUser} = require('../db/queries/user-queries')
+const { allUsers, findUserById, updateUserEmail, updateUserName, updateUserPassword, updateUserImageURL, createUser, deleteUser} = require('../db/queries/user-queries')
 
 const authenticateUser = (userId, password) => {
   const user = findUserById(userId);
@@ -9,7 +9,14 @@ const authenticateUser = (userId, password) => {
     return true
   }
   return false
-}
+};
+
+const isLoggedIn = (cookie) => {
+  if (cookie === true) {
+    return true
+  }
+  return false
+};
 
 
 
