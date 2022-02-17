dbe87296d1f2810d03c8cd1b77e224aa43eac12d
Noah V <noah@noahvandenberg.com>
2022-02-17 14:10:01 -0800

    linted helper functions

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,36 +1,35 @@
-const { allMaps, findMapsByUser, findMapById, updateMapTitle, updateMapDescription, updateMapImageUrl, createMap, deleteMap } = require('../db/queries/map-queries')
-const { allPoints, findPointsByUser, findPointById, updatePointTitle, updatePointDescription, updatePointImageURL, updatePointLocation, createPoint, deletePoint} = require('../db/queries/point-queries')
-const { allUsers, findUserById, updateUserEmail, updateUserName, updateUserPassword, updateUserImageURL, createUser, deleteUser} = require('../db/queries/user-queries')
+const { allMaps, findMapsByUser, findMapById, updateMapTitle, updateMapDescription, updateMapImageUrl, createMap, deleteMap } = require('../db/queries/map-queries');
+const { allPoints, findPointsByUser, findPointById, updatePointTitle, updatePointDescription, updatePointImageURL, updatePointLocation, createPoint, deletePoint} = require('../db/queries/point-queries');
+const { allUsers, findUserById, updateUserEmail, updateUserName, updateUserPassword, updateUserImageURL, createUser, deleteUser} = require('../db/queries/user-queries');
 
 const authenticateUser = (userId, password) => {
   const user = findUserById(userId);
   // - Implement Bcrypt Later
   if (user.password === password) {
-    return true
+    return true;
   }
-  return false
+  return false;
 };
 
 const isLoggedIn = (cookie) => {
   if (cookie === true) {
-    return true
+    return true;
   }
-  return false
+  return false;
 };
 
 const randomSelection = (arr, count) => {
   const randomArr = [];
   for (let i = 0; i < count; i++) {
-    randomChoice = Math.floor(Math.random() * arr.length);
-    randomMapArry.push(arr[randomChoice]);
+    const randomChoice = Math.floor(Math.random() * arr.length);
+    randomArr.push(arr[randomChoice]);
   }
   return randomArr;
-}
-
+};
 
 
 module.exports = {
   authenticateUser,
   isLoggedIn,
   randomSelection,
-}
+};
