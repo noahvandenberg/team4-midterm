3590a0fb68bb37a3e4897588d1d4431f8b386c4d
Thomas Ngo <thomasngo818@gmail.com>
2022-02-18 02:56:32 +0000

    Update map route to render specific map to page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,6 +1,6 @@
 const { allPoints, mapPointsByUser, mapPoints, findPoint } = require('../db/queries/point-queries')
 const { allUsers, findUser } = require('../db/queries/user-queries')
-const { allMaps, findUserMaps} = require('../db/queries/map-queries')
+const { allMaps, findUserMaps } = require('../db/queries/map-queries')
 
 
 module.exports = (router, db) => {
@@ -11,27 +11,27 @@ module.exports = (router, db) => {
   });
 
   // Serve The Login Page
-  router.get("/login", (req,res) => {
+  router.get("/login", (req, res) => {
 
   });
 
   // Server The General Map Page
-  router.get("/maps", async (req,res) => {
+  router.get("/maps", async (req, res) => {
 
   });
 
   // Serve A Specific Map Page
-  router.get("/maps:map_id", (req,res) => {
-
+  router.get("/maps:map_id", (req, res) => {
+    res.render("../views/map")
   });
 
   // Login A User
-  router.post("/login", (req,res) => {
+  router.post("/login", (req, res) => {
 
   });
 
   // Logout A Users
-  router.post("/logout", (req,res) => {
+  router.post("/logout", (req, res) => {
 
   });
 
